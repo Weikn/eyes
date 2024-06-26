e6fd88c5f7d8de8fff731fa0f5ab9e241a721bf7
aweikn <w_k_guo@126.com>
2024-06-27 00:55:24 +0800

    0627

--- eyes/spiders/biquge.py
+++ eyes/spiders/biquge.py
@@ -1,12 +1,13 @@
 import scrapy
 
-
+##笔趣阁 检索小说返回并下载
 class BiqugeSpider(scrapy.Spider):
-    name = 'biquge'
-    start_urls = ['https://www.xskkk.com/']
+    name = 'm.biqu520'
+    start_urls = ['http://m.biqu520.net/']
 
     def parse(self, response):
         res = response.xpath("/html/body/div[2]/div/div/form/div/input")
         
         print(res)
+        print(response.body)
 
--- eyes/spiders/mysplash.py
+++ eyes/spiders/mysplash.py
@@ -16,7 +16,7 @@ class JsSpider(scrapy.Spider):
         for url in self.start_urls:
             yield SplashRequest(url, self.parse_result, endpoint='render.html',
                                 args=splash_args)
-
+  
     def parse_result(self, response):
         print('----------使用splash爬取京东网首页异步加载内容-----------')
         guessyou = response.xpath('//*[@id="navitems-group1"]/li[1]/a').extract_first()
