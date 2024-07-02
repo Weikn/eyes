89ce70f0df22459b1a0ecf5cd61769899c21d41b
aweikn <w_k_guo@126.com>
2024-07-03 00:09:53 +0800

    第一次 js

--- eyes/spiders/biquge.py
+++ eyes/spiders/biquge.py
@@ -9,16 +9,23 @@ class BiqugeSpider(scrapy.Spider):
     # 千万别用这个 要不重写方法不生效  巨坑  
     #allowed_domains =" "
 
-    start_urls = ['https://www.baidu.com/']
+    start_urls = ['https://m.xbiqugew.com/']
     lua_source = """
         function main(splash, args)
+            assert(splash:autoload("https://code.jquery.com/jquery-2.1.3.min.js"))
             assert(splash:go(args.url))
-            assert(splash:wait(0.5))
-            return {
-                html = splash:html(),
-                png = splash:png(),
-                har = splash:har(),
-            }
+            assert(splash:wait(1))
+            -- local el = assert(splash:select('/html/body/div[3]/form/table/tbody/tr/td[2]/input'))
+            -- local version = splash:evaljs("$.fn.jquery")
+            -- local version = splash:evaljs("$.c_big_border")
+            -- return 'te'
+            local get_bbox = splash:jsfunc([[
+                function() {
+                var el = document.getElementsByClassName("c_big_border")[0].innerHTML
+                return el;
+                }
+            ]])
+            return get_bbox()
         end
     """
     # 重写start_requests
@@ -37,3 +44,6 @@ class BiqugeSpider(scrapy.Spider):
     def parse(self, resp):
         print("result ----------------------------")
         print(resp.body)
+        print(resp.text)
+        print(type(resp))
+        print(resp)
